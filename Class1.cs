90f8e44647ae10c2195e7b3e6470a89cc5d8bb1e
Lukas Rieger <lr@live.de>
2017-07-15 00:39:44 +0200

    c# project

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace paket_csharp_test
+{
+    public class Class1
+    {
+        public static void Stuff()
+        {
+            var x = Newtonsoft.Json.DateFormatHandling.IsoDateFormat;
+        }
+    }
+}
